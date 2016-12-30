68ee56d0e077670c51536535a4d6c5f3ee048d5d
ray lin <ray lin>
2016-12-29 20:54:42 -0800

    routes chapter finished

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,15 +3,16 @@ import { HeroesComponent } from './heroes.component';
 
 @Component({
     selector : 'my-app',
+    moduleId : module.id,
     template : `
     <h1>{{title}}</h1>
-   <nav>
-     <a routerLink="/dashboard">Dashboard</a>
-     <a routerLink="/heroes">Heroes</a>
-   </nav>
-   <router-outlet></router-outlet>
+  <nav>
+    <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+    <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+  </nav>
+  <router-outlet></router-outlet>
     `,
-    
+    styleUrls : ['app.component.css']
 })
 export class AppComponent {
     title: string = 'Tour of Heroes';
